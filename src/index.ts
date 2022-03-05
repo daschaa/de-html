a5728d7f02e315d0787c9d27404b471490e5465c
Joshua Weber <josh@joshuaw.de>
2022-03-05 17:06:44 +0100

    :white_check_mark: Adds tests for all available web symbols

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import * as characters from './characterMap.json'
+
 export const decode = (text: string) => {
-    return text.replace("&#35;", "#");
+    let outText = text;
+    for(const symbol of characters) {
+        outText = outText.replace(symbol.Code, symbol.Symbol.toString());
+    }
+    return outText;
 };
