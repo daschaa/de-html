17d1bdea9ce29ae57716a5781d9611d51107dfc4
Joshua Weber <josh@joshuaw.de>
2022-03-06 02:21:31 +0100

    :art: Removes js from package json

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import * as characters from './characterMap.json'
+import * as characters from './characterMap.json';
 
 export const decode = (text: string) => {
-    let outText = text;
-    for(const symbol of characters) {
-        outText = outText.replace(symbol.Code, symbol.Symbol.toString());
-    }
-    return outText;
+  let outText = text;
+  for (const symbol of characters) {
+    outText = outText.replace(symbol.Code, symbol.Symbol.toString());
+  }
+  return outText;
 };
